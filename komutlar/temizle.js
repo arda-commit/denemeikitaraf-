1fc136b07948aa0a4c9795893f53c1a375d8faa0
Glitch (dust-holy-cafe) <none>
2021-04-12 19:43:32 +0000

    📮🎹 Checkpoint ./komutlar/temizle.js:29534031/1242 ./komutlar/temizle.js copy:29534031/2 ./komutlar/sil.js:29534031/36

--- komutlar/temizle.js
+++ komutlar/temizle.js
@@ -1,1 +1,20 @@
-714756114520211486
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args) => {
+
+if(!args[0]) return message.channel.send("Lütfen Silinecek Sayı Miktarını Giriniz.").then(m => m.delete({timeout: 5000}));
+  
+message.channel.bulkDelete(args[0]);
+  
+return message.channel.send(`**${args[0]}** Adet Mesaj Başarıyla Temizlendi.`).then(m => m.delete({timeout: 5000}));
+};
+
+exports.conf = {
+enabled: true,
+guildOnly: true,
+aliases: []
+}
+
+exports.help = {
+name: 'temizle'
+};
