5daff4b422f53ab41ac915906ef8c7c5fc6cdde1
Glitch (dust-holy-cafe) <none>
2021-04-15 20:13:49 +0000

    📼🙏 Checkpoint ./komutlar/yardim.js:29534031/461

--- komutlar/yardim.js
+++ komutlar/yardim.js
@@ -1,6 +1,6 @@
 exports.run = (client, message, args) => {
 const Discord = require('discord.js')
-const embed = Discord.MessageEmbed()
+const embed = new Discord.MessageEmbed()
 .setTitle('Yardım menüsü')
 .setDescription(`
 !!seviye-log (seviye logu ayarlarsınız)
@@ -12,14 +12,23 @@ const embed = Discord.MessageEmbed()
 !!düello (etiketlediğiniz kişiyle düello yaparsınız)
 !!rank (rankınıza bakarsınız)
 !!temizle (belirttiğiniz miktarda mesaj siler)
-!!
+!!herkeserolver (herkese etiketlenen rolü verir)
+!!kullanıcıbilgi (kullanıcının bilgilerine bakarsınız)
+!!deprembilgi (son 10 depremi ve bilgilerini sıralar)
+!!dosyaoluştur (dosya oluşturursunuz)
+--------------------------------------------------------
+        -------sahip yardım---------
+!!load (bir komutu yükler)
+!!eval (komut denersiniz)
+!!reboot (botu yeniden başlatır)
 `)
+message.channel.send(embed)
 };
 
 exports.conf = {
   enabled: true,
   guildOnly: false,
-  aliases: [""],
+  aliases: ["komutlar"],
   permLevel: 0
 };
 
