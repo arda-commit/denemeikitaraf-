7db93a788c58ae1ca1cea2f349768331b268ed33
Glitch (dust-holy-cafe) <none>
2021-04-15 17:11:42 +0000

    🏄🕌 Checkpoint ./komutlar/sunucubilgi.js:29534031/1779 ./komutlar/load.js:29534031/792

---
+++ komutlar/load.js
@@ -0,0 +1,27 @@
+exports.run = (client, message, args) => {
+    var command = args[0];
+    message.channel.send("`" + command + "` adlı komut yükleniyor...")
+      .then(m => {
+        client.load(command)
+          .then(() => {
+            m.edit("`" + command + "` adlı komut başarıyla yüklendi.");
+          })
+          .catch(e => {
+            m.edit(`Komut yüklenirken bir hata oluştu: ${command}\n\`\`\`${e.stack}\`\`\``);
+          if(message.author.id ==! "714756114520211486") return message.channel.send('sahibim değilsin')
+          });
+      });
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: false,
+  aliases: [],
+  permLevel: 4
+};
+
+exports.help = {
+  name: 'load',
+  description: 'Yeni eklenen komutu yükler.',
+  usage: 'load <komut adı>'
+};
