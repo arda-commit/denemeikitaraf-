57d401ae4b6a6ade201f9fd7cc7f395875626e7a
Glitch (dust-holy-cafe) <none>
2021-04-21 17:25:44 +0000

    🌾🍰 Checkpoint ./komutlar/hgbbkanal.js:24539357/843 ./package.json:24539357/4736

---
+++ komutlar/hgbbkanal.js
@@ -0,0 +1,31 @@
+const discord = require('discord.js');
+const db = require("quick.db")
+
+exports.run = async(client, message, args) => {
+let enginar = message.mentions.channels.first()
+if(!enginar) return message.channel.send('Lütfen bir kanal belirt!')
+
+db.set(`hgbbmesaj_${message.guild.id}`, enginar.id)
+
+const embed = new discord.MessageEmbed()
+.setTitle('Başarılı!')
+.setDescription(`Hoşgeldin mesajı kanalı başarılı ile <#${enginar.id}> olarak ayarlandı!`)
+return message.channel.send(embed)
+
+
+
+
+};
+exports.conf = {
+    enabled: true,  
+    guildOnly: false, 
+    aliases: [], 
+    permLevel: 0
+  };
+  
+  exports.help = {
+    name: 'hg-mesajı',
+    description: 'taslak', 
+    usage: 'captcha'
+  };
+  
