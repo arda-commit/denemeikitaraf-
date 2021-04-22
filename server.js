56b532bda09f1dc8ebd4aca6c9deeb90d026377c
Glitch (dust-holy-cafe) <none>
2021-04-22 09:57:50 +0000

    👶🍱 Checkpoint ./server.js:24539357/1327

--- server.js
+++ server.js
@@ -250,4 +250,73 @@ if (message.content === prefix + 'fakeayrıl') { // Buraya ne yazarsanız yazdı
   client.emit('guildMemberRemove', message.member || await message.guild.fetchMember(message.author));
     }
 });
+client.on('ready', () => {
+  console.log(`Bot Aktif :  ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
+client.on('message', message => { 
+	
+	function rasteleSembol(uzunluk, semboller) {
+
+var maske = '';
+
+if (semboller.indexOf('a') > -1) maske += 'abcdefghijklmnopqrstuvwxyz';
+
+if (semboller.indexOf('A') > -1) maske += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+if (semboller.indexOf('0') > -1) maske += '0123456789';
+
+
+var sonuc = '';
+
+ 
+
+for (var i = uzunluk; i > 0; --i) 
+
+{
+
+sonuc += maske[Math.floor(Math.random() * maske.length)];
+
+}
+
+return sonuc;
+
+}
+
+function spamla(){
+	message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
+}
+
+	if (message.channel.id = message.channel.id){ //NİTROYU ATACAĞI KANALIN İDSİ
+		if(message.content.startsWith( prefix + "nitro")){ //ELLEME ÇALIŞMAZ :D
+		try{
+		var veri = message.content.toString();
+		var veriSn = veri.substring(10)
+ 		var saniye = parseInt(veriSn);
+ 		var saniye1 = saniye * 1000;
+		if (saniye > 1){
+		setInterval(spamla, saniye1);
+		}
+		
+		else{
+			message.reply("Bir Hata Oluştu. Bu Arada En Az 2 Saniye Girmelisin");
+		}
+		}
+		catch(err){
+			message.channel.send("Teknik bir hata oluştu!");
+		}
+		}
+		}
+ 	
+
+
+});
 
