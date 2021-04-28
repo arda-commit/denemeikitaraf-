ca95ea67351cb209c8466dfe1d474b2dc60b5114
Glitch (dust-holy-cafe) <none>
2021-04-28 17:24:16 +0000

    🌲🍤 Checkpoint ./ekonomi/cf.js:24539357/77

--- ekonomi/cf.js
+++ ekonomi/cf.js
@@ -1,7 +1,7 @@
 module.exports = {
   name:"cf",
   code:`
-  $if[$randomText[a;b]==a]
+  $if[$random[1;100]==50]
   Maalesef kaybettiniz. $message kadar paranızı kaybettiniz
   $setUserVar[coin;$sub[$getUserVar[coin];$message[1]];$authorID]
   $else
@@ -10,6 +10,6 @@ module.exports = {
   $endif
   $onlyIf[$checkContains[.; ;,;a;b;c;ç;d;e;f;g;ğ;h;ı;i;j;k;l;m;n;o;ö;p;r;s;ş;t;;u;ü;v;y;z;x;q;A;B;C;Ç;D;E;F;G;Ğ;H;I;İ;J;K;L;M;N;O;Ö;P;R;S;Ş;T;U;Ü;V;Y;Z;X;Q]==true;lütfen doğru bir sayı girin! örnek 200]
   $onlyIf[$message=>$getUserVar[coin];o kadar paran yok!]
-  $onlyIf[$message]
+  $onlyIf[$message=<0;0ın üstünde 1 rakam girin!]
   `
   }
