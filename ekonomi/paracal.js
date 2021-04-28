799efde8808686f8a72ed611a86155a48c6e0054
Glitch (dust-holy-cafe) <none>
2021-04-28 18:20:57 +0000

    🎙👶 Checkpoint ./ekonomi/paracal.js:24539357/160

--- ekonomi/paracal.js
+++ ekonomi/paracal.js
@@ -8,11 +8,11 @@ module.exports = {
   $else
   kişiye farkettirmeden biraz coin çalabildin! çaldığın coinin miktarı: $random[1;1000]
   $setUserVar[coin;$sum[$getUserVar[coin];$random[1;1000];$authorID]
-  $setUserVar[coin;$sub[$getUserVar[coin];$message[1]];$mentioned[1]]
+  $setUserVar[coin;$sub[$getUserVar[coin];$random[1;1000];$mentioned[1]]
   $endif
-  $onlyIf[$mentioned[1]==;birini etiketle]
+  $argsCheck[1;birini etiketle]
   $onlyIf[$mentioned[1]==$authorID;kendinden mi para çalacaksın?]
-  $onlyIf[$getUserVar[coin;$mentio]]
+  $onlyIf[$getUserVar[coin;$mentioned[1]=<300;etiketlediğin kişinin en az 300 parası olması lazım!]]
   $globalCooldown[5s;5 saniye bekle]
   `
   }
