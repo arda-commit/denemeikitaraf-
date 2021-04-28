e6cc5e0ca04131ff5a4a8277b9a5c76d697611a6
Glitch (dust-holy-cafe) <none>
2021-04-28 19:19:08 +0000

    📖😇 Checkpoint ./ekonomi/cf.js:24539357/58

--- ekonomi/cf.js
+++ ekonomi/cf.js
@@ -9,8 +9,8 @@ module.exports = {
   $setUserVar[coin;$sum[$getUserVar[coin];$message];$authorID]
   $endif
   $onlyIf[$checkContains[.; ;,;a;b;c;ç;d;e;f;g;ğ;h;ı;i;j;k;l;m;n;o;ö;p;r;s;ş;t;;u;ü;v;y;z;x;q;A;B;C;Ç;D;E;F;G;Ğ;H;I;İ;J;K;L;M;N;O;Ö;P;R;S;Ş;T;U;Ü;V;Y;Z;X;Q]==true;lütfen doğru bir sayı girin! örnek 200]
-  $onlyIf[$message>$getUserVar[coin];o kadar paran yok!]
-  $onlyIf[$message<0;0ın üstünde 1 rakam girin!]
+  $onlyIf[$getUserVar[coin]<$message;o kadar paran yok!]
+  $onlyIf[$message>0;0ın üstünde 1 rakam girin!]
   $argsCheck[1;bir sayı gir]
   $suppressErrors[Lütfen doğru bir sayı girin!]
   $globalCooldown[3s;3 saniye bekle]
