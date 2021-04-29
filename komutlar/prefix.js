1c500d4d43d58011f3a03968a47cbeae80b447ba
Glitch (dust-holy-cafe) <none>
2021-04-29 08:19:59 +0000

    💼❤️ Checkpoint ./komutlar/prefix.js:24539357/1137

--- komutlar/prefix.js
+++ komutlar/prefix.js
@@ -1,12 +1,18 @@
 const Discord = require("discord.js");
-
-module.exports.run = async (client, message) => {
-  
-   const prefixayarlamesaj = new Discord.MessageEmbed()
+const db = require("quick.db")
+module.exports.run = async (client, message, args) => {
+ let prefix = args[0] 
+   const prefixayarlamesajhata = new Discord.MessageEmbed()
    .setDescription(`
    Bir prefix girin! örnek: m!
    `)
-
+   if(!prefix) return message.channel.send(prefixayarlamesajhata)
+if(prefix > 3) return message.channel.send("prefixin uzunluğu en fazla 3 olabilir!")
+  const prefixayarlandi = new Discord.MessageEmbed()
+  .setTitle("prefix ayarlandı")
+  .setDescription(`prefix başarıyla \`${prefix}\` olarak ayarlandı!`)
+  message.channel.send(prefixayarlandi)
+  db.set(`prefix_${message.guild.id}`, prefix)
 };
 module.exports.conf = {
   enabled: true,
