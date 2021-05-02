bab9e2634f73fe0aa1d55165f6482c0ca220bd08
Glitch (dust-holy-cafe) <none>
2021-05-02 09:14:09 +0000

    👁👸 Checkpoint ./events/ready.js:24539357/1946 ./server.js:24539357/65

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,6 @@ const chalk = require("chalk");
 const moment = require("moment");
 const Discord = require("discord.js");
 const ayarlar = require("../ayarlar.json");
-const db = require('quick.db');
 
 var prefix = ayarlar.prefix;
 
@@ -15,9 +14,8 @@ module.exports = client => {
       client.user.username
     } ismi ile giriş yapıldı!`
   );
-    }
-module.exports = client => {
-var oyun = [
+  client.user.setStatus("dnd");
+  var oyun = [
     ".",
     "..",
     "...",
@@ -29,14 +27,13 @@ var oyun = [
     "....",
     "...",
     "..",
-    "."
+    ".",
   
   ];
-client.user.setStatus("dnd")
+
   setInterval(function() {
     var random = Math.floor(Math.random() * (oyun.length - 0 + 1) + 0);
 
     client.user.setActivity(oyun[random], "");
   }, 2 * 2500);
-client.user.setStatus("dnd")
-  }
+};
