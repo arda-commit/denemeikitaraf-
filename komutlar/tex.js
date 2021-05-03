441279374df3f4f7e6eea2b6e1d338fd30e673fc
Glitch (dust-holy-cafe) <none>
2021-05-03 11:22:05 +0000

    💡🌇 Checkpoint ./komutlar/tex.js:24539357/1141

--- komutlar/tex.js
+++ komutlar/tex.js
@@ -52,7 +52,9 @@ exports.run = function(client, message, args) {
     ]
 if(tex === "shelly") return message.channel.send("https://cdn.discordapp.com/attachments/838725812278132761/838725896226603038/shelly_v2_01.png")
   if(tex === "colt") return message.channel.send("https://cdn.discordapp.com/attachments/838725812278132761/838733146257883156/colt_tex.png")
-  if(tex === "nita") return message.channel.send("")
+  if(tex === "nita") return message.channel.send("https://cdn.discordapp.com/attachments/838725812278132761/838734466380857364/nita_tex.png")
+  if(tex === "bull") return message.channel.send("https://cdn.discordapp.com/attachments/838725812278132761/838736382767136768/bull_tex_01.png")
+  if(tex === "jessie") return message.channe.send("https://cdn.discordapp.com/attachments/838725812278132761//jessie_tex.png")
   
 };  
 
